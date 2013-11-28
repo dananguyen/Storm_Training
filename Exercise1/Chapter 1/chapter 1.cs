48a87085b12fb66f4719209413eae28fbab65943
dananguyen <dana_nguyen@ymail.com>
2013-11-27 21:30:26 -0800

    Exercise1

---
+++ Exercise1/Chapter 1/chapter 1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace First_c_sharp_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; //variable for storing string value
+            Console.WriteLine("Please Enter Your Good Name"); //displaying value for entering value
+            name = Console.ReadLine(); //accepts and hold values in name variables
+            Console.WriteLine("Welcome {0} in your first c sharp program", name);  //displaying output
+            Console.ReadLine(); //holds console screen
+        }
+    }
+}
---
+++ Exercise1/Chapter3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result; //creating integer typevalue
+            
+            Console.WriteLine("Please Enter Value");
+
+            num1 = Int32.Parse(Console.ReadLine()); //accepting value in num1
+
+            Console.WriteLine("Please Enter Second Value"); //Displays Message
+
+            num2 = Int32.Parse(Console.ReadLine());
+
+            result = num1 + num2; //processing value
+
+            Console.WriteLine("Add of {0} and {1} is {2}", num1, num2, result); //Output
+
+            Console.ReadLine();
+        }
+    }
+}
