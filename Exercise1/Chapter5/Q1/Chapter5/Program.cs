5868d29d9e996c7a003143666afedec15a3d0aef
dananguyen <dana_nguyen@ymail.com>
2013-11-28 10:42:01 -0800

    Chapter5

---
+++ Exercise1/Chapter5/Q1/Chapter5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Chapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string city;
+            string street;
+            int house;
+            int pin;
+
+            Console.WriteLine("Enter your name: \n");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your city: \n");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter your street address: \n");
+            street = Console.ReadLine();
+
+            Console.WriteLine("Enter your house number: \n");
+            house = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your pin: \n");
+            pin = Int32.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("====================");
+            Console.WriteLine("Your Complete Address");
+            Console.WriteLine("====================");
+
+
+            Console.WriteLine("Name = {0}", name);
+            Console.WriteLine("City = {0}", city);
+            Console.WriteLine("Street = {0}", street);
+            Console.WriteLine("House = {0}", house);
+            Console.WriteLine("Pin = {0}", pin);
+            Console.WriteLine("=====================");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise1/Chapter5/Q2/Q2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string university;
+            string classroom;
+            sbyte age;
+            int roll;
+
+            Console.WriteLine("Enter your student name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter what university you attend:");
+            university = Console.ReadLine();
+
+            Console.WriteLine("Enter your classroom");
+            classroom = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            age = sbyte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your roll number: \n");
+            roll = Int32.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("======================");
+            Console.WriteLine("Your Student Info");
+            Console.WriteLine("========================");
+
+            Console.WriteLine("Name = {0}", name);
+            Console.WriteLine("University = {0}", university);
+            Console.WriteLine("Classroom = {0}", classroom);
+            Console.WriteLine("Age = {0}", age);
+            Console.WriteLine("Roll = {0}", roll);
+
+            Console.ReadLine();
+        }
+    }
+}
